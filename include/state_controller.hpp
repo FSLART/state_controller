d9f14ebe76b4b65efef25223fc90e8209f54c118
André Lopes <andreglopes2005@gmail.com>
2024-11-13 19:40:49 +0000

    Try to make can work

--- include/state_controller.hpp
+++ include/state_controller.hpp
@@ -7,16 +7,32 @@
 #include <iomanip>
 #include <iostream>
 #include <sstream>
-#include "exceptions.hpp"
-#include "openssl/sha.h"
+#include "linux/can.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <thread>
+#include <unistd.h> 
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/bool.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float64.hpp"
 
 #include "lart_msgs/msg/state.hpp"
 #include "lart_msgs/msg/mission.hpp"
 #include "lart_msgs/msg/as_status.hpp"
+#include "lart_msgs/msg/dynamics_cmd.hpp"
+#include "./Can-Header-Map/CAN_asdb.h"
+#include "./Can-Header-Map/CANOPEN_maxondb.h"
+
+#define T24E_CAN_INTERFACE "can0"
+
+//#define RES_READY_CAN_ID 0x0B//see real id
 
 class StateController : public rclcpp::Node
 {
@@ -25,35 +41,46 @@ public:
 
 private:
   // Function declarations
-  std::string compute_sha256(const std::string &data);
-  void publish_handshake();
-  void acuStateCallback(const lart_msgs::msg::State::SharedPtr msg);
-  void handshakeCallback(const lart_msgs::msg::ASStatus::SharedPtr msg);
-  void ready_state();
-  void ebsStatusCallback(const std_msgs::msg::Bool::SharedPtr msg);
-  void goSignalCallback(const std_msgs::msg::Bool::SharedPtr msg);
+  void read_can_frame();
   void missionFinishedCallback(const lart_msgs::msg::ASStatus::SharedPtr msg);
-  void standstillCallback(const std_msgs::msg::Bool::SharedPtr msg);
   bool valid_state(lart_msgs::msg::State msg);
+  void send_can_frames();
+  void send_can_frame(struct can_frame frame);
+  void handle_can_frame(struct can_frame frame);
+  void spacCallback(const lart_msgs::msg::DynamicsCMD::SharedPtr msg);
+  void emergencyCallback(const std_msgs::msg::Bool::SharedPtr msg);
+  void inspectionSteeringAngleCallback(const std_msgs::msg::Float64::SharedPtr msg);
+  void maxon_activation();
 
-  // Member variables
-  lart_msgs::msg::State state_msg;
+
+  // class variables
+  int s=-1;//socket descriptor
+
+  lart_msgs::msg::State state_msg;//save the current state
+  lart_msgs::msg::DynamicsCMD dynamics_msg;//save the current dynamics command
   bool mission_finished;
-  int handshake_recived;
-  std::chrono::steady_clock::time_point last_publish;
-  std::chrono::steady_clock::time_point ready_start_time_;
+  std::chrono::steady_clock::time_point ready_change;//time the state was changed to ready
+  bool res_ready;
 
-  // Subscriptions
-  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr emergency_brake_sub_;
-  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr go_signal_sub_;
-  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr standstill_sub_;
+  std::mutex state_mutex;
+
+  // mission controller subscription
   rclcpp::Subscription<lart_msgs::msg::ASStatus>::SharedPtr mission_finished_sub_;
-  rclcpp::Subscription<lart_msgs::msg::ASStatus>::SharedPtr handshake_sub_;
-  rclcpp::Subscription<lart_msgs::msg::State>::SharedPtr acu_state_sub_;
 
-  // Publishers
+  //spac subscription
+  rclcpp::Subscription<lart_msgs::msg::DynamicsCMD>::SharedPtr spac_sub_;
+
+  // emergency stop subscription
+  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr emergency_sub_;
+
+  // state publisher to pc
   rclcpp::Publisher<lart_msgs::msg::State>::SharedPtr state_publisher_;
-  rclcpp::Publisher<lart_msgs::msg::ASStatus>::SharedPtr handshake_publisher_;
+
+  //mission_publisher
+  rclcpp::Publisher<lart_msgs::msg::Mission>::SharedPtr mission_publisher_;
+
+  //inspection steering angle publisher
+  rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr inspection_steering_angle_sub_;
 };
 
 #endif
--- src/state_controller.cpp
+++ src/state_controller.cpp
@@ -2,197 +2,231 @@
 
 using std::placeholders::_1;
 
-StateController::StateController() : Node("state_controller"), mission_finished(false)
-{
-    handshake_sub_ = this->create_subscription<lart_msgs::msg::ASStatus>("/acu_origin/system_status/critical_as", 10, std::bind(&StateController::handshakeCallback, this, _1));
-
+StateController::StateController() : Node("state_controller"){
     mission_finished_sub_ = this->create_subscription<lart_msgs::msg::ASStatus>("/pc_origin/system_status/critical_as", 10, std::bind(&StateController::missionFinishedCallback, this, _1));
 
-    go_signal_sub_ = this->create_subscription<std_msgs::msg::Bool>("GoSignal", 10, std::bind(&StateController::goSignalCallback, this, _1));
+    emergency_sub_ = this->create_subscription<std_msgs::msg::Bool>("/pc_origin/emergency", 10, std::bind(&StateController::emergencyCallback, this, _1));//decide the topic name
 
-    emergency_brake_sub_ = this->create_subscription<std_msgs::msg::Bool>("/acu_origin/system_status/critical_as/ebs_brk", 10, std::bind(&StateController::ebsStatusCallback, this, _1));
-
-    standstill_sub_ = this->create_subscription<std_msgs::msg::Bool>("Standstill", 10, std::bind(&StateController::standstillCallback, this, _1));
+    spac_sub_ = this->create_subscription<lart_msgs::msg::DynamicsCMD>("spacccccc", 10, std::bind(&StateController::spacCallback, this, _1));//change for right topic name
 
     state_publisher_ = this->create_publisher<lart_msgs::msg::State>("/pc_origin/system_status/critical_as/state", 10);
 
-    acu_state_sub_ = this->create_subscription<lart_msgs::msg::State>("/acu_origin/system_status/critical_as/state", 10, std::bind(&StateController::acuStateCallback, this, _1));
+    mission_publisher_ = this->create_publisher<lart_msgs::msg::Mission>("/pc_origin/system_status/critical_as/mission", 10);
 
-    handshake_publisher_ = this->create_publisher<lart_msgs::msg::ASStatus>("/pc_origin/system_status/critical_as", 10);
+    inspection_steering_angle_sub_ = this->create_subscription<std_msgs::msg::Float64>("steering_cmd", 10, std::bind(&StateController::inspectionSteeringAngleCallback, this, _1));
 
     state_msg.data = lart_msgs::msg::State::OFF; // initialize state as off
     
-    publish_handshake();
-}
+    mission_finished = false;
 
-std::string StateController::compute_sha256(const std::string &data)
-{
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256(reinterpret_cast<const unsigned char *>(data.c_str()), data.size(), hash);
+    res_ready = false;
 
-    std::stringstream ss;
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i)
-    {
-        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    // create a socket
+	if((this->s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+		RCLCPP_ERROR(this->get_logger(), "Failed to create socket: %s", strerror(errno));
+		return;
+	}
+
+    //define can interface
+    struct ifreq ifr;
+    strcpy(ifr.ifr_name, T24E_CAN_INTERFACE);
+    ioctl(this->s, SIOCGIFINDEX, &ifr);
+    
+    
+
+    // bind the socket to the CAN interface
+    struct sockaddr_can addr;
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = ifr.ifr_ifindex;
+    if(bind(this->s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        RCLCPP_ERROR(this->get_logger(), "Failed to bind socket: %s", strerror(errno));
+        return;
     }
-    return ss.str();
+
+
+    // create a thread to read CAN frames
+	std::thread read_can_thread(&StateController::read_can_frame, this);
+	read_can_thread.detach();
+
+    std::thread send_can_thread(&StateController::send_can_frames, this);
+    send_can_thread.detach();
+
+    maxon_activation();
+
+    
+    
 }
 
-void StateController::publish_handshake()
-{
-    lart_msgs::msg::ASStatus handshake_msg;
+void StateController::maxon_activation(){
+    std::cout<<"maxon activation"<<std::endl << std::flush;
 
-    auto now = std::chrono::system_clock::now();
-    auto epoch = now.time_since_epoch();
-    auto unix_time = std::chrono::duration_cast<std::chrono::seconds>(epoch).count();
+    struct can_frame frame;
+    frame.can_id = 0;
+    frame.can_dlc = 8;
 
-    std_msgs::msg::String hash_msg;
-    hash_msg.data = compute_sha256(std::to_string(unix_time));
+    frame.data[0] = 0x00; //turn on the maxon
+    frame.data[1] = 0x05;
+    send_can_frame(frame);
+    
 
-    handshake_msg.timestamp = unix_time;
-    handshake_msg.hash = hash_msg.data;
-    handshake_publisher_->publish(handshake_msg);
+    frame.data[0]=0x80;//pre op mode
+    frame.data[1]=0x05;
+    send_can_frame(frame);
+    
+
+    frame.data[0]=0x01;//op mode
+    frame.data[1]=0x05;
+    send_can_frame(frame);
+
+    frame.can_id = 0x205;
+    frame.data[0]=0x06;
+    frame.data[1]=0x05;
+    send_can_frame(frame);
+
+    frame.data[0]=0x0F;
+    frame.data[1]=0x05;
+    send_can_frame(frame);
+
 }
 
-void StateController::acuStateCallback(const lart_msgs::msg::State::SharedPtr msg)
-{
-    if (msg->data == lart_msgs::msg::State::EMERGENCY)
-    {
-        state_msg.data = lart_msgs::msg::State::EMERGENCY;
-        RCLCPP_INFO(this->get_logger(), "Emergency state activated");
-        state_publisher_->publish(state_msg);
-    }
+void StateController::inspectionSteeringAngleCallback(const std_msgs::msg::Float64::SharedPtr msg){//to test the maxon with the jetson
+    // Handle inspection steering angle callback
+    float angle = msg->data;
+
+    struct can_frame frame;
+    frame.can_id = 0x405;
+    frame.can_dlc = 8;
+    MAP_ENCODE_AS_STEERING_ANGLE_ACTUAL(frame.data, angle);
+    this->send_can_frame(frame);
+    std::cout<<"angle: "<<angle<<std::endl;
 }
 
-void StateController::handshakeCallback(const lart_msgs::msg::ASStatus::SharedPtr msg)
-{
-    if (msg->hash.empty() && msg->timestamp == 0)
-    {
-        handshake_recived = 1;
-        ready_state();
-    }
-    else
-    {
-        auto now = std::chrono::system_clock::now();
-        auto epoch = now.time_since_epoch();
-        auto unix_time = std::chrono::duration_cast<std::chrono::seconds>(epoch).count();
+void StateController::spacCallback(const lart_msgs::msg::DynamicsCMD::SharedPtr msg){
+    struct can_frame frame;
+    frame.can_id = CAN_AS_DYNAMICS_ONE;
+    frame.can_dlc = 8;
+    
 
-        std_msgs::msg::String verify_recived_msg;
-        verify_recived_msg.data = compute_sha256(std::to_string(unix_time));
-        if (msg->hash.compare(verify_recived_msg.data) == 0 && msg->timestamp == unix_time)
+    // Handle spac callback
+}
+
+void StateController::missionFinishedCallback(const lart_msgs::msg::ASStatus::SharedPtr msg){
+    // Handle mission finished callback from mission controller
+}
+
+void StateController::emergencyCallback(const std_msgs::msg::Bool::SharedPtr msg){
+    //hand emergency from pc pipeline
+}
+
+// Send frame
+void StateController::send_can_frames(){
+    struct can_frame frame;
+    frame.can_id = CAN_AS_STATUS;
+    frame.can_dlc = 8;
+    while(rclcpp::ok()){
         {
-            handshake_recived = 1;
-            ready_state();
-        }
-        else
-        {
-            handshake_recived = 0;
-            auto now = std::chrono::steady_clock::now();
-            if (std::chrono::duration_cast<std::chrono::seconds>(now - last_publish).count() >= 1)
-            {
-                publish_handshake();
-                last_publish = std::chrono::steady_clock::now();
+            std::lock_guard<std::mutex> guard(this->state_mutex);
+            if(res_ready && std::chrono::steady_clock::now() - ready_change > std::chrono::seconds(5)&& state_msg.data ==lart_msgs::msg::State::READY){
+                this->state_msg.data = lart_msgs::msg::State::DRIVING;
+                this->state_publisher_->publish(this->state_msg);
+                MAP_ENCODE_AS_STATE(frame.data, state_msg.data);
+                this->send_can_frame(frame);
+            }else{
+                this->res_ready = false;
             }
         }
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
 }
 
-void StateController::ready_state()
-{
-    if (state_msg.data == lart_msgs::msg::State::OFF)
-    {
-        state_msg.data = lart_msgs::msg::State::READY;
-        ready_start_time_ = std::chrono::steady_clock::now();
-        RCLCPP_INFO(this->get_logger(), "Ready state activated");
-        state_publisher_->publish(state_msg);
-    }
-    else if (state_msg.data == lart_msgs::msg::State::EMERGENCY)
-    {
-        throw EmergencyException("Emergency state detected");
-    }
-    else if (!valid_state(state_msg))
-    {
-        throw BadStateException("Bad state detected");
+void StateController::send_can_frame(struct can_frame frame){
+    if(write(this->s, &frame, sizeof(frame)) < 0) {
+        RCLCPP_ERROR(this->get_logger(), "Failed to send CAN frame: %s", strerror(errno));
     }
 }
 
-void StateController::ebsStatusCallback(const std_msgs::msg::Bool::SharedPtr msg)
-{
-    if (msg->data && state_msg.data != lart_msgs::msg::State::EMERGENCY)
-    {
-        state_msg.data = lart_msgs::msg::State::EMERGENCY;
-        RCLCPP_INFO(this->get_logger(), "Emergency Brake system activated");
-        state_publisher_->publish(state_msg);
+// Handle CAN frame
+void StateController::handle_can_frame(struct can_frame frame){
+    switch (frame.can_id){
+        case (0x185):
+            //maxon feedback
+            {uint32_t statusword1 = MAP_DECODE_PDO_TXONE_STATUSWORD(frame.data);
+            uint32_t mode = MAP_DECODE_PDO_TXONE_MODES_OF_OPERATION(frame.data);
+            uint32_t error_code = MAP_DECODE_PDO_TXONE_ERROR_CODE(frame.data);
+            // Handle maxon feedback
+            std::cout<<"statusword: "<<statusword1<<std::endl;
+            std::cout<<"mode: "<<mode<<std::endl;
+            std::cout<<"error_code: "<<error_code<<std::endl;}
+
+            break;
+        case (0x285):
+            //maxon feedback
+            {uint32_t target_position = MAP_DECODE_PDO_TXTWO_TARGET_POSITION(frame.data);
+            uint32_t target_speed = MAP_DECODE_PDO_TXTWO_TARGET_SPEED(frame.data);
+            // Handle maxon feedback
+            std::cout<<"target_position: "<<target_position<<std::endl;
+            std::cout<<"target_speed: "<<target_speed<<std::endl;}
+
+            break;
+        case (0x385):
+            //maxon feedback
+            {uint32_t statusword2 = MAP_DECODE_PDO_TXTHREE_STATUSWORD(frame.data);
+            uint32_t actual_position = MAP_DECODE_PDO_TXTHREE_ACTUAL_POSITION(frame.data);
+            uint32_t actual_moment = MAP_DECODE_PDO_TXTHREE_ACTUAL_MOMENT(frame.data);
+            // Handle maxon feedback
+            std::cout<<"statusword: "<<statusword2<<std::endl;
+            std::cout<<"actual_position: "<<actual_position<<std::endl;
+            std::cout<<"actual_moment: "<<actual_moment<<std::endl;}
+            break;
+        case (0x485):
+            //maxon feedback
+            {uint32_t statusword3 = MAP_DECODE_PDO_TXFOUR_STATUSWORD(frame.data);
+            uint32_t actual_speed = MAP_DECODE_PDO_TXFOUR_ACTUAL_SPEED(frame.data);
+            uint32_t actual_pwm_duty = MAP_DECODE_PDO_TXFOUR_ACTUAL_PWM_DUTY(frame.data);
+            // Handle maxon feedback
+            std::cout<<"statusword: "<<statusword3<<std::endl;
+            std::cout<<"actual_speed: "<<actual_speed<<std::endl;
+            std::cout<<"actual_pwm_duty: "<<actual_pwm_duty<<std::endl;}
+
+            break;
+        case CAN_AS_STATUS:
+            {uint32_t status = MAP_DECODE_AS_STATE(frame.data);
+            uint32_t mission = MAP_DECODE_AS_MISSION(frame.data);//publish mission to computer
+
+            if(status == lart_msgs::msg::State::READY && state_msg.data != lart_msgs::msg::State::READY){
+                ready_change = std::chrono::steady_clock::now(); //save the time the state was changed to ready
+                this->state_msg.data = lart_msgs::msg::State::READY;
+            }}
+            // Handle ACU state frame
+            break;
+        /*case RES_READY_CAN_ID:
+            // Handle car ready frame, not to implement yet, test first without verifications
+            break;*/
     }
 }
 
-void StateController::goSignalCallback(const std_msgs::msg::Bool::SharedPtr msg)
-{
-    if (msg->data && state_msg.data == lart_msgs::msg::State::READY)
-    {
-        auto now = std::chrono::steady_clock::now();
-        auto ready_duration = std::chrono::duration_cast<std::chrono::seconds>(now - ready_start_time_).count();
-
-        if (ready_duration >= 5)
-        {
-            state_msg.data = lart_msgs::msg::State::DRIVING;
-            RCLCPP_INFO(this->get_logger(), "Driving state activated");
-            state_publisher_->publish(state_msg);
-        }
-    }
+void StateController::read_can_frame(){
+    while(rclcpp::ok()) {
+		struct can_frame frame;
+		int nbytes = read(this->s, &frame, sizeof(frame));
+		if(nbytes < 0) {
+			RCLCPP_ERROR(this->get_logger(), "Failed to read CAN frame: %s", strerror(errno));
+			return;
+		}
+		handle_can_frame(frame);
+	}
 }
 
-void StateController::missionFinishedCallback(const lart_msgs::msg::ASStatus::SharedPtr msg)
-{
-    if (msg->state.data == lart_msgs::msg::State::FINISH)
-    {
-        mission_finished = true;
-    }
-    else
-    {
-        mission_finished = false;
-    }
-}
-
-void StateController::standstillCallback(const std_msgs::msg::Bool::SharedPtr msg)
-{
-    if (msg->data && mission_finished && state_msg.data != lart_msgs::msg::State::FINISH)
-    {
-        state_msg.data = lart_msgs::msg::State::FINISH;
-        RCLCPP_INFO(this->get_logger(), "Mission finished, state changed to Finish");
-        state_publisher_->publish(state_msg);
-    }
-}
-
-bool StateController::valid_state(lart_msgs::msg::State msg)
-{
+bool StateController::valid_state(lart_msgs::msg::State msg){
     return (msg.data == lart_msgs::msg::State::OFF || msg.data == lart_msgs::msg::State::READY || msg.data == lart_msgs::msg::State::DRIVING || msg.data == lart_msgs::msg::State::EMERGENCY || msg.data == lart_msgs::msg::State::FINISH);
 }
 
 int main(int argc, char *argv[])
 {
-    try
-    {
         rclcpp::init(argc, argv);
         rclcpp::spin(std::make_shared<StateController>());
         rclcpp::shutdown();
 
-    }
-    catch (const EmergencyException &e)
-    {
-        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Emergency Exception: %s", e.what());
-        return EXIT_FAILURE;
-    }
-    catch (const BadStateException &e)
-    {
-        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Bad State Exception: %s", e.what());
-        return EXIT_FAILURE;
-    }
-    catch (const std::exception &e)
-    {
-        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Exception: %s", e.what());
-        return EXIT_FAILURE;
-    }
 
     return 0;
 }
